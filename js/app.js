44c7de5fdccc0c109642a1b095682eaa810684d1
Smurrowz <nigeldn1995@gmail.com>
2022-08-30 12:38:48 +0200

    added info button

--- js/app.js
+++ js/app.js
@@ -4,13 +4,17 @@ const app = new Vue({
   data: {
     user: {
       name: 'Nigel',
+      userMenu: false,
       avatar: '_7'
     },
 
     userNewMessage: '',
     contactSearch: '',
     activeContact: 0,
-
+    msgInfo: {
+      data: '',
+      autore : ''
+    },
     contacts: [
       {
         name: 'RRR',
@@ -23,18 +27,21 @@ const app = new Vue({
             text: 'OH LO ZI É USCITO IL CAPITOLO',
             status: 'sent',
             activeMenu: false,
+            activeInfo: false,
           },
           {
             date: '12/04/2022 06:33:02',
             text: 'JA VELOCE LOGGA',
             status: 'sent',
             activeMenu: false,
+            activeInfo: false,
           },
           {
             date: '12/04/2022 06:38:18',
             text: 'EDEAI ARRIVO',
             status: 'received',
             activeMenu: false,
+            activeInfo: false,
           }
         ],
       },
@@ -50,18 +57,21 @@ const app = new Vue({
             text: 'oh ci vogliamo beccare sabato a lanciano?',
             status: 'sent',
             activeMenu: false,
+            activeInfo: false,
           },
           {
             date: '12/04/2022 06:33:02',
             text: 'si dai, ci sto',
             status: 'received',
             activeMenu: false,
+            activeInfo: false,
           },
           {
             date: '12/04/2022 06:38:18',
             text: 'vabbé dai. Allora parto per le 9:30 come al solito',
             status: 'sent',
             activeMenu: false,
+            activeInfo: false,
           }
         ],
       },
@@ -76,18 +86,21 @@ const app = new Vue({
             text: 'minchia zio sono ubriachissimo',
             status: 'received',
             activeMenu: false,
+            activeInfo: false,
           },
           {
             date: '12/04/2022 06:33:02',
             text: 'Albé ma vai a dormire dio bono',
             status: 'sent',
             activeMenu: false,
+            activeInfo: false,
           },
           {
             date: '12/04/2022 06:38:18',
             text: 'hai ragione zio...',
             status: 'received',
             activeMenu: false,
+            activeInfo: false,
           }
         ],
       },
@@ -102,12 +115,14 @@ const app = new Vue({
             text: 'Giovedí vuoi venire al quizzettone?',
             status: 'sent',
             activeMenu: false,
+            activeInfo: false,
           },
           {
             date: '12/04/2022 06:38:18',
             text: 'Boh non lo so, ti faccio sapere',
             status: 'received',
             activeMenu: false,
+            activeInfo: false,
           }
         ],
       },
@@ -122,10 +137,11 @@ const app = new Vue({
     sendMessage() {
       if (this.userNewMessage.trim() !== '') {
         const newMessage = {
-          date: '26/08/2022 12:25:30',
+          date: dayjs().format('DD/MM/YYYY hh:mm:ss'),
           text: this.userNewMessage,
           status: 'sent',
           activeMenu: false,
+          activeInfo: false,
         }
         this.contacts[this.activeContact].messages.push(newMessage)
         for (let i = 0; i < this.contacts[this.activeContact].messages.length; i++) {
@@ -142,10 +158,11 @@ const app = new Vue({
     newReceiveMessage() {
 
       const newReceivedMessage = {
-        date: '26/08/2022 12:25:31',
+        date: dayjs().format('DD/MM/YYYY hh:mm:ss'),
         text: 'ok',
         status: 'received',
         activeMenu: false,
+        activeInfo: false,
       }
       this.contacts[this.activeContact].messages.push(newReceivedMessage)
 
@@ -176,11 +193,18 @@ const app = new Vue({
       }
     },
     popMenu(index) {
-
+      for (let i = 0; i <  this.contacts[this.activeContact].messages.length; i++) {
+        const el =  this.contacts[this.activeContact].messages[i];
+        el.activeMenu = false
+      }
+     
       this.contacts[this.activeContact].messages[index].activeMenu = !this.contacts[this.activeContact].messages[index].activeMenu;
+    },
+    popInfo(index) {
+
+      this.contacts[this.activeContact].messages[index].activeInfo = !this.contacts[this.activeContact].messages[index].activeInfo;
 
     },
-
     deleteMessage(index) {
 
       this.contacts[this.activeContact].messages.splice(index, 1);
@@ -199,7 +223,22 @@ const app = new Vue({
 
       
     },
-
+    showInfo(index){
+      this.msgInfo.autore = ''
+      this.msgInfo.data = ''
+      if(this.contacts[this.activeContact].messages[index].status === 'sent'){
+        // console.log(this.user.name) 
+        this.msgInfo.autore = this.user.name
+        
+      }else{
+        // console.log(this.contacts[this.activeContact].name)
+        this.msgInfo.autore = this.contacts[this.activeContact].name
+        
+      }
+      this.msgInfo.data = this.contacts[this.activeContact].messages[index].date
+      
+    }
+    
 
 
   },
